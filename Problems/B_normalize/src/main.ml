745d79b836ebeda892897ab4a713d4c7ae927b98
Tina Smirnova <tinanight98@gmail.com>
2019-02-23 16:41:58 +0300

    Compilled

--- Problems/B_normalize/src/main.ml
+++ Problems/B_normalize/src/main.ml
@@ -7,33 +7,36 @@ let (>>) x f = f x;;
 module VarSet = Set.Make(String);;
 module VarMap = Map.Make(String);;
 
-let need_to_change var = VarSet.exists var;;
+let var_set = VarSet.empty;;
 
-let change_var var =
-  let new_var = var + "\'" in
+let need_to_change var = VarSet.mem var var_set;;
+
+let rec change_var var =
+  let new_var = var ^ "\'" in
     if need_to_change new_var then change_var new_var else new_var
     ;;
 
-(* TODO: check if it's nessecary to substitute again *)
-let substitute expr_of_abstraction var expr_to_subs =
-  let rec subs e = match e with
-  | Var v -> if v == var then (* TODO: change variable in abstraction to e *)
-  | Appl (l,r) -> subs l; subs r
-  | Abstr (v,r) -> subs r;
-  in subs expr_of_abstraction
-  ;;
-
-let try_to_apply l r = match l with
-  | Abstr (v,rr) -> substitute rr v r; (* rr - expression without variable of abstraction *)
-  | _ -> 
+let rec try_to_apply l r = match l with
+  | Abstr (v,rr) -> (* substitute rr v r;  rr - expression without variable of abstraction *)
+    let substitute expr_of_abstraction var expr_to_subs =
+      let rec subs e = match e with
+      | Var vs -> if vs == var then e else Var(vs);
+      | Appl (ls,rs) -> try_to_apply (subs ls) (subs rs);
+      | Abstr (vs,rs) -> if vs == var then Abstr(vs, rs) else Abstr(vs, subs rs); (* TODO: check closest lambda *)
+      in subs expr_of_abstraction;
+    in substitute rr v r;
+  | _ -> Appl(l, r);
   ;;
 
 let string_of_tree tree =
   let buf = create 1000 in
   let rec s_t t = match t with
-    | Var v -> add_string buf v
-    | Appl (l,r) -> add_string buf (try_to_apply l v);
-    | Abstr (v,r) -> VarSet.add v; s_t r;
+    | Var v -> add_string buf v;
+    | Appl (l,r) -> add_string buf "("; let loc_appl = try_to_apply l r in
+                                        if loc_appl == Appl(l, r)
+                                        then (s_t l; add_string buf " "; s_t r;)
+                                        else s_t loc_appl; add_string buf ")";
+    | Abstr (v,r) -> VarSet.add v var_set; add_string buf "(\\"; add_string buf v; add_string buf ".("; s_t r; add_string buf ")";
   in s_t tree;
   contents buf;;
 
